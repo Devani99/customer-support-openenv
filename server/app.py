cdd140babf4c94f60f71bb3178fb81ca5ac80d9b
Devani Rushit <155089098+Devani99@users.noreply.github.com>
2026-04-07 15:40:14 +0530

    Add files via upload

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from env.environment import SupportEnv
 from env.models import Action
+import uvicorn
 
 app = FastAPI()
 env = SupportEnv()
@@ -25,3 +26,11 @@ def step(action: Action):
 @app.get("/state")
 def state():
     return env.get_state()
+
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
